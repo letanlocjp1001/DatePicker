049b04e227758537c5d7fa53274ec31dd20e38f1
letanloc <letanlocjp1001@gmail.com>
2023-02-02 17:47:05 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+
+import DatePicker from './components/DatePicker'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={{
+          uri: 'https://images.unsplash.com/photo-1577998474517-7eeeed4e448a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        }}
+        resizeMode='cover'
+        style={{ flex: 1, justifyContent: 'center' }}
+      >
+        <Text style={styles.txt}>生年月日</Text>
+        <View style={styles.box}>
+          <DatePicker
+            defaultDate={'1994-01-10'}
+            onDateChange={(value) => console.log(value)}
+          />
+        </View>
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  txt: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 30,
+  },
+  box: {
+    alignItems: 'center',
+  },
+})
